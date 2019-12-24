ArrowOS-Devices/android_device_motorola_potter

potter: Correct define libinit variable
Fixes:
		error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//:libinit_potter"

BoardConfig.mk
@@ -69,8 +69,8 @@ USE_DEVICE_SPECIFIC_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_potter
-TARGET_RECOVERY_DEVICE_MODULES := libinit_potter
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_potter
+TARGET_RECOVERY_DEVICE_MODULES := //$(DEVICE_PATH):libinit_potter
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237

xayron/android_device_yureka2

BoardConfig.mk
@@ -186,8 +186,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_co
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_YUREKA2
-TARGET_RECOVERY_DEVICE_MODULES := libinit_YUREKA2
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_YUREKA2
+TARGET_RECOVERY_DEVICE_MODULES := //$(DEVICE_PATH):libinit_YUREKA2
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true